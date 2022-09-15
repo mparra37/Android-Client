ffbab91f9f1bd3022d6de9702f4f2ede921f8928
Mario Parra <marioparra375@gmail.com>
2022-09-15 13:03:26 -0700

    Add second java acivity

--- app/src/main/java/parra/mario/mobileclient/MainActivity.kt
+++ app/src/main/java/parra/mario/mobileclient/MainActivity.kt
@@ -2,10 +2,51 @@ package parra.mario.mobileclient
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import parra.mario.mobileclient.databinding.ActivityMainBinding
+import java.io.DataOutputStream
+import java.net.Socket
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var et_message: EditText
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val myThread: MyThread = MyThread()
+
+        Thread(myThread).start()
+
+    }
+
+    class MyThread : Runnable{
+        var msg: String = ""
+        lateinit var socket: Socket
+        lateinit var dos: DataOutputStream
+
+        override fun run() {
+            socket = Socket("158.97.91.129", 5555)
+            dos = DataOutputStream(socket.getOutputStream())
+            dos.writeUTF(msg)
+            dos.close()
+            dos.flush()
+            socket.close()
+
+        }
+
+        fun sendMsg(msg: String){
+            this.msg = msg
+            run()
+        }
+
+        fun btnClickSend(v: View){
+
+        }
     }
 }
